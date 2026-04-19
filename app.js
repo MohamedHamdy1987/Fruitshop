fc15809b49428c354963e848a21dacf4e6e0b683
MohamedHamdy1987 <shahat10101048@gmail.com>
2026-04-19 18:05:22 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,28 +1,14 @@
-window.addEventListener("DOMContentLoaded", () => {
+window.onload = function () {
+
+  alert("DOM LOADED");
 
   const app = document.getElementById("app");
 
   document.querySelectorAll("[data-nav]").forEach(btn => {
-    btn.addEventListener("click", () => {
-      const page = btn.getAttribute("data-nav");
-
-      if (page === "dashboard") {
-        app.innerHTML = "<h2>Dashboard شغال</h2>";
-      }
-
-      else if (page === "customers") {
-        app.innerHTML = "<h2>صفحة العملاء</h2>";
-      }
-
-      else if (page === "sales") {
-        app.innerHTML = "<h2>صفحة المبيعات</h2>";
-      }
-
-      else {
-        app.innerHTML = `<h2>${page}</h2>`;
-      }
-
-    });
+    btn.onclick = function () {
+      const page = this.getAttribute("data-nav");
+      app.innerHTML = `<h2>${page}</h2>`;
+    };
   });
 
-});
+};
